b2b014af0eedff828604baae72f9b9b884e4df5f
Ray Linares <rayl77@gmail.com>
2018-06-09 00:30:10 -0500

    Created layout for Detail Activity. Added parseSandwichJSON method to JSONUtils. Updated DetailActivity to populate UI.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +17,26 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView mSandwichName_tv;
+    private TextView mSandwichAlsoKnownAs_tv;
+    private TextView mSandwichOrigin_tv;
+    private TextView mSandwichIngredients_tv;
+    private TextView mSandwichDescription_tv;
+    private ImageView mSandwichImage_iv;
+    private TextView mAKALabel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mSandwichName_tv = findViewById(R.id.sandwich_name_tv);
+        mSandwichAlsoKnownAs_tv = findViewById(R.id.aka_tv);
+        mSandwichOrigin_tv = findViewById(R.id.origin_tv);
+        mSandwichIngredients_tv = findViewById(R.id.ingredients_tv);
+        mSandwichDescription_tv = findViewById(R.id.description_tv);
+        mSandwichImage_iv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +59,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(mSandwichImage_iv);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +72,61 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    //Method used to populate text views in Detail Activity with parsed JSON
+    private void populateUI(Sandwich sandwich) {
 
+        //Populate main name text view with Main Name
+        mSandwichName_tv.setText(sandwich.getMainName());
+
+        //Checking if Also Known As List is empty or not.
+        if (sandwich.getAlsoKnownAs() != null && sandwich.getAlsoKnownAs().size() > 0) {
+            StringBuilder akaString = new StringBuilder();
+
+            //Loop to append all items in list to the string
+            for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
+                akaString.append(sandwich.getAlsoKnownAs().get(i));
+
+                //Will append a comma after every item in the list as long as it is not the last item
+                if (i != sandwich.getAlsoKnownAs().size()-1) {
+                    akaString.append(", ");
+                }
+            }
+            //If not empty, populate Also Known As field with the String built by appending each item in list
+            mSandwichAlsoKnownAs_tv.setText(akaString.toString());
+
+            //If List was empty, populate text view with an Unknown message
+        }else mSandwichAlsoKnownAs_tv.setText(R.string.unknown_message);
+
+
+        //If Place of Origin is given, then populate Origin text view
+        if (sandwich.getPlaceOfOrigin() != null && sandwich.getPlaceOfOrigin().length() > 0) {
+            mSandwichOrigin_tv.setText(sandwich.getPlaceOfOrigin());
+
+            //If empty, populate Origin text view with an Unknown message
+        }else mSandwichOrigin_tv.setText(R.string.unknown_message);
+
+
+        //Populate Description text view
+        mSandwichDescription_tv.setText(sandwich.getDescription());
+
+
+        //Checking if Ingredients list is empty or not
+        if (sandwich.getIngredients() != null && sandwich.getIngredients().size() > 0) {
+            StringBuilder ingredientsStringBuilder = new StringBuilder();
+
+            //Loop to append all items in list to string
+            for (int i = 0; i < sandwich.getIngredients().size(); i++) {
+                ingredientsStringBuilder.append(sandwich.getIngredients().get(i));
+
+                //Will append a comma after every item in the list as long as it is not the last item
+                if (i != sandwich.getIngredients().size()-1){
+                    ingredientsStringBuilder.append(", ");
+                }
+            }
+            //If not empty, populate Ingredients text view with String built by appending each item in list
+            mSandwichIngredients_tv.setText(ingredientsStringBuilder.toString());
+
+            //If List was empty, populate text view with an Unknown message
+        }else mSandwichIngredients_tv.setText(R.string.unknown_message);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,60 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public final static String NAME_KEY = "name";
+    public final static String MAIN_NAME_KEY = "mainName";
+    public final static String AKA_KEY = "alsoKnownAs";
+    public final static String ORIGIN_KEY = "placeOfOrigin";
+    public final static String DESCRIPTION_KEY = "description";
+    public final static String IMAGE_KEY = "image";
+    public final static String INGREDIENTS_KEY = "ingredients";
+
+    public static Sandwich parseSandwichJson(String sandwichJSON) {
+        try {
+
+            JSONObject rootObject = new JSONObject(sandwichJSON);
+            JSONObject name = rootObject.getJSONObject(NAME_KEY);
+            String mainName = name.getString(MAIN_NAME_KEY);
+
+            JSONArray alsoKnownAs = name.optJSONArray(AKA_KEY);
+            List<String> allAKA = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAs.length(); i++){
+                String akaName = alsoKnownAs.getString(i);
+                allAKA.add(akaName);
+            }
+
+            String origin = rootObject.getString(ORIGIN_KEY);
+            String description = rootObject.getString(DESCRIPTION_KEY);
+            String image = rootObject.getString(IMAGE_KEY);
+
+            JSONArray ingredients = rootObject.optJSONArray(INGREDIENTS_KEY);
+            List<String> allIngredients = new ArrayList<>();
+            for (int i = 0; i < ingredients.length(); i++){
+                String ingredientName = ingredients.getString(i);
+                allIngredients.add(ingredientName);
+            }
+
+            return new Sandwich(mainName, allAKA, origin, description, image, allIngredients);
+
+        }catch (JSONException e){
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the Sandwich JSON results", e);
+            return null;
+        }
     }
 }
